88ed7c0c3616b8ae8c7bdb73be922f463185c5e2
Alves731 <rozinaria.hta@gmail.com>
2026-03-24 16:34:58 -0300

    Feat: Create Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react'
-import { ThemeProvider } from 'styled-components'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import { ThemeProvider } from "styled-components"
+import ReactDOM from "react-dom/client"
 import App from './App.tsx'
-import { GlobalStyle} from '@styles/globalStyle.ts'
-import { lighTheme} from '@styles/theme.ts'
+import { GlobalStyle, lighTheme } from './styles/'
 
 
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <ThemeProvider theme={lighTheme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
-  </StrictMode>,
+  </React.StrictMode>
 )
